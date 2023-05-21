2082c1ff9243a847ac22f5ae243fea6484e2d97c
andrsand <angorko@list.ru>
2023-05-21 13:00:10 +0300

    Created a dispatch function with action addTodo

--- src/components/Form.js
+++ src/components/Form.js
@@ -13,11 +13,14 @@ const Form = () => {
             text: todoValue,       // в поле text лежит значение из стейта todoValue
             completed: false,
         }
+        dispatch(addTodo(todo)) // вызываем функцию dispatch с экшном addTodo где параметром будет наша todo
+        setTodoValue('') // чистим нашу todo 
     }
     return (
         <form className='w-full flex' onSubmit={(e) => e.preventDefault()}>
             <input
                 type='text'
+                value={todoValue} // значение из стейта
                 placeholder='Type something...'
                 onChange={(e) => setTodoValue(e.target.value)} // при изменении поля формы помещем value в стейт todoValue
                 className='w-full p-1 focus:outline-none focus:border-lime-500 focus: border-2 placeholder:text-sm'
@@ -25,6 +28,7 @@ const Form = () => {
             <button
                 type='submit'
                 className='shrink-0 bg-lime-300  hover:bg-lime-400 transition-all px-3 text-sm'
+                onClick={() => addTodoHandler()} // вешаем на onClick наш addTodoHandler
             >
                 Submit
             </button>
