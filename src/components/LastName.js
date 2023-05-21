10914f1541d4e2ee37fa5e6e9df32116f9e058d9
andrsand <angorko@list.ru>
2023-05-21 09:03:14 +0300

    Creating a changed state Receipt in the Last Name field

--- src/components/LastName.js
+++ src/components/LastName.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const LastName = () => {
-    return <div className='font-bold'>Last Name</div>
+    const lastName = useSelector((state) => state.user.lastName)  //  useSelector - хук, state -глобальный объект, user.lastName - поле из файла userSlice.js
+    return <div className='font-bold'>{lastName}</div>             // на вывод в div помещаем вышесозданную переменную lastname
 }
+
 
 export default LastName
